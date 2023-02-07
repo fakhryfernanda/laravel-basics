46dadaead2eb15309a1be4d6c9fb01412d6c3990
fakhryfernanda <mff023@gmail.com>
2023-02-07 14:07:41 +0700

    Route basic (optional parameter)

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,27 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Return view
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// Return view cara kedua
+Route::view('/', 'welcome');
+
+
+// Return string
+Route::get('/hello', fn() => 'Hello World');
+
+// Redirect
+Route::redirect('/greet', '/hello');
+
+// Route with required parameter
+Route::get('/user/{id}', fn($id) => 'User '.$id);
+
+// Route with two paramaters
+Route::get('/posts/{post}/comments/{comment}', fn($post, $comment) => 'Post '.$post.' Comment '.$comment);
+
+// Route with optional parameter
+Route::get('/product/{id?}', fn($id = null) => 'Product ID: '.$id);
